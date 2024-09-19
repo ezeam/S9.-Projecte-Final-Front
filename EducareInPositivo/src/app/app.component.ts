d4a9b2b9d64c8949ac8f108b90e390bea3999131
Ester <ester.zea.maldonado@gmail.com>
2024-09-19 11:38:08 +0200

    WIP-migrados header y footer, trabajando en la home

--- EducareInPositivo/src/app/app.component.ts
+++ EducareInPositivo/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 @Component({
