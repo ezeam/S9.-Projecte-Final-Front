7ff815c9ce260d51b99827516dcf94c23d8375f1
Ester <ester.zea.maldonado@gmail.com>
2025-01-15 16:01:23 +0100

    Mensaje aviso cookies ahora se muetra/esconde según true/false almacenado en las cookies, ya no se almancena en localstorage

--- EducareInPositivo/src/app/components/cookie-notice/cookie-notice.component.ts
+++ EducareInPositivo/src/app/components/cookie-notice/cookie-notice.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-cookie-notice',
   standalone: true,
-  imports: [CommonModule], // Importa aquí los módulos que uses en el componente
+  imports: [CommonModule],
   templateUrl: './cookie-notice.component.html',
   styleUrls: ['./cookie-notice.component.scss']
 })
@@ -16,14 +16,32 @@ export class CookieNoticeComponent {
   }
 
   checkCookie(): void {
-    const cookiesAccepted = localStorage.getItem('cookiesAccepted');
-    if (cookiesAccepted === 'true') {
+    const cookiesAcceptedEducare = this.getCookie('cookiesAcceptedEducare');
+    if (cookiesAcceptedEducare === 'true') {
       this.showCookieNotice = false;
     }
   }
 
   acceptCookies(): void {
-    localStorage.setItem('cookiesAccepted', 'true');
+    this.setCookie('cookiesAcceptedEducare', 'true', 365);
     this.showCookieNotice = false;
+  }
+
+  // Función para obtener el valor de una cookie
+  getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts.pop()?.split(';').shift() || null;
+    }
+    return null;
+  }
+
+  // Función para establecer una cookie
+  setCookie(name: string, value: string, days: number): void {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
   }
 }
